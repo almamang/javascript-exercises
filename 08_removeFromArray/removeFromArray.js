8dde54e3bada79c39e7aabc51e5235cd494b8f46
almamang <48696353+almamang@users.noreply.github.com>
2025-08-03 16:51:26 +0700

    Add solutions to 08 & 09

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    let newArray = [];
+    array.forEach(element => {
+        if (!items.includes(element)) {
+            newArray.push(element);
+        };
+        
+    });
+    return newArray
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(fromNum, toNum) {
+    let sum = 0
+    if (!Number.isInteger(fromNum) || !Number.isInteger(toNum)) return 'ERROR';
+    if (fromNum < 0 || toNum < 0) return 'ERROR';
+    if (fromNum > toNum) {
+        let temp = fromNum;
+        fromNum = toNum;
+        toNum = temp;
+    };
 
+    for (let i = fromNum; i <= toNum; i++) {
+        sum += i;
+    };
+    return sum;
 };
 
 // Do not edit below this line
